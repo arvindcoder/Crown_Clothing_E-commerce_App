68265d56443df85248e50f7412fb4f1dde0a09cc
Arvind Pal <arvindpal@Arvinds-MacBook-Air.local>
2023-09-27 23:49:55 +0530

    added checkout component and increment/decrement functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 const addCartItem = (cartItems, productToAdd) => {
+
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
   );
@@ -15,11 +16,31 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+ const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+
+if (existingCartItem.quantity ===1){
+  return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id);
+}
+
+
+return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
   cartCount: 0
 });
 
@@ -37,7 +58,11 @@ setCartCount(newCartCount);
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+  const removeItemToCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem (cartItems, cartItemToRemove));
+  };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart,  cartItems, cartCount, removeCartItem };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
 //---------------------------
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,32 @@
+import { UserContext, useContext } from "react";
+
+import { CartContext, CartCount } from "../../contexts/cart.context";
+
+import "./checkout.styles.scss";
+
+const Checkout = () => {
+  const { cartItems, addItemToCart, removeItemToCart } = useContext(CartContext);
+
+
+  return(
+    <div>
+        <h1>i am the checkout page</h1>
+    <div>
+    {cartItems.map((cartItem) => {
+        const {id, name, quantity } = cartItem;
+      return (
+        <div key={id}>
+          <h2>{name}</h2>
+          <span>{quantity}</span>
+          <br />
+          <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
+          <br />
+          <span onClick={()=> addItemToCart(cartItem)}>increment</span>
+        </div>
+      );
+    })}
+  </div>
+  </div>
+  );
+}
+export default Checkout;
