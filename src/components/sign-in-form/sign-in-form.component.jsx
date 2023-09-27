17d4611b6ffb4ec26280a75181055b2bd71b3318
Arvind Pal <arvindpal@Arvinds-MacBook-Air.local>
2023-09-27 22:50:23 +0530

    added in our addToCart feature

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -25,8 +25,8 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-  createUserDocumentFromAuth(user);
+  await signInWithGooglePopup();
+
   };
 
   const handleSubmit = async (event) => {
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,16 +1,100 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
-    isCartOpen: false,
-    setIsCartOpen: () => {
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0
+});
 
-    }
-})
+export const CartProvider = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0)
 
-export const CartProvider = ({children}) => {
+  useEffect(() => {
+const newCartCount = cartItems.reduce((total, cartItem)=>total + cartItem.quantity, 0)
+setCartCount(newCartCount);
+  }, [cartItems])
 
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen,setIsCartOpen };
-    return <CartContext.Provider value={value}>{children}</CartContext.Provider>
-    
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
+//---------------------------
+// import React, { createContext, useState, useContext } from "react";
+
+// // Create a context for the cart
+// export const CartContext = createContext();
+
+// // Custom hook to easily access the cart context
+// export function useCart() {
+//   return useContext(CartContext);
+// }
+
+// export const CartProvider = ({ children }) => {
+//   const [isCartOpen, setIsCartOpen] = useState(false);
+//   const [cartItems, setCartItems] = useState([]);
+
+//   // Function to add an item to the cart
+//   const addItemToCart = (productToAdd) => {
+//     setCartItems((prevCartItems) => [...prevCartItems, productToAdd]);
+//   };
+
+//   // Function to remove an item from the cart
+//   const removeItemFromCart = (productIdToRemove) => {
+//     setCartItems((prevCartItems) =>
+//       prevCartItems.filter((item) => item.id !== productIdToRemove)
+//     );
+//   };
+
+//   // Function to clear the cart
+//   const clearCart = () => {
+//     setCartItems([]);
+//   };
+
+//   // Calculate the total number of items in the cart
+//   const cartItemCount = cartItems.length;
+
+//   // Calculate the total cost of items in the cart
+//   const cartTotal = cartItems.reduce(
+//     (total, item) => total + item.price,
+//     0
+//   );
+
+//   const value = {
+//     isCartOpen,
+//     setIsCartOpen,
+//     cartItems,
+//     addItemToCart,
+//     removeItemFromCart,
+//     clearCart,
+//     cartItemCount,
+//     cartTotal,
+//   };
+
+//   return (
+//     <CartContext.Provider value={value}>
+//       {children}
+//     </CartContext.Provider>
+//   );
+// };
